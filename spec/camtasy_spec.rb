74f15c9665be9fcfdd143b0a8aa932b5d3a5a8b6
mo khan <mo@mokhan.ca>
2014-08-28 21:31:54 -0600

    include bin and specs.

---
+++ spec/camtasy_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Camtasy do
+  it 'has a version number' do
+    expect(Camtasy::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
