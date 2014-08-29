47e3c962337f920233bdf5db6dba76fc12eb103e
mo khan <mo@mokhan.ca>
2014-08-28 21:16:37 -0600

    add script to take pictures in a loop.

---
+++ preview.rb
@@ -0,0 +1,17 @@
+require 'av_capture'
+require 'io/console'
+
+session = AVCapture::Session.new
+dev = AVCapture.devices.find(&:video?)
+
+session.run_with(dev) do |connection|
+  loop do
+    case $stdin.getch
+    when 'q' then break # quit when you hit 'q'
+    else
+      IO.popen("open -g -f -a /Applications/Preview.app", 'w') do |f|
+        f.write connection.capture
+      end
+    end
+  end
+end
